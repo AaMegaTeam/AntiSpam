e7f786fb6b6696eec119235dc8507cc4b5b5ad6c
AaMegaTeam <hamed.dehghani321@gmail.com>
2016-10-11 05:32:17 -0700

    Update Admin.lua

--- plugins/Admin.lua
+++ plugins/Admin.lua
@@ -261,8 +261,8 @@ end
 	if matches[1] == 'reload' then
 		receiver = get_receiver(msg)
 		reload_plugins(true)
-		post_msg(receiver, "Reloaded!", ok_cb, false)
-		return "Reloaded!"
+		post_msg(receiver, "", ok_cb, false)
+		return "<i>Bot Reloaded By:</i>@"..msg.from.username.."\n<b>All Plugins Reloaded!</b>\n<i>All Changes Saved!</i>\n<b>Mega Maximus</b> <i>Is Ready Now!!!</i>"
 	end
 	--[[*For Debug*
 	if matches[1] == "vardumpmsg" and is_admin1(msg) then
